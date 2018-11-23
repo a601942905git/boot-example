383fbd38d32d1a979f7525b94019651a3c7471a9
lipeng <lipeng@jd.com>
2018-11-23 14:21:01 +0800

    Spring Boot启动类中添加访问信息

--- boot-example-start/src/main/java/com/boot/example/StartApplication.java
+++ boot-example-start/src/main/java/com/boot/example/StartApplication.java
@@ -1,8 +1,13 @@
 package com.boot.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * com.boot.example.StartApplication
@@ -12,9 +17,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.boot.example")
+@Slf4j
 public class StartApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        Environment environment = SpringApplication.run(StartApplication.class, args).getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        log.info("=================================");
+        log.info("      项目名称:" + environment.getProperty("spring.application.name"));
+        log.info("      项目访问地址:" + ip + ":" + environment.getProperty("server.port"));
+        log.info("=================================");
     }
 }
