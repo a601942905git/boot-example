9e00413970c04e86ac12f8eace690db6a3667cfa
lipeng <lipeng@jd.com>
2018-11-30 11:09:40 +0800

    Spring Boot自定义异常以及异常处理器

--- boot-example-start/src/main/java/com/boot/example/controller/mock/MockController.java
+++ boot-example-start/src/main/java/com/boot/example/controller/mock/MockController.java
@@ -2,10 +2,13 @@ package com.boot.example.controller.mock;
 
 import com.boot.example.core.entity.mock.Mock;
 import com.boot.example.core.service.mock.MockService;
+import com.boot.example.exception.CustomerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 /**
  * com.boot.example.controller.mock.MockController
@@ -22,6 +25,9 @@ public class MockController {
 
     @RequestMapping("/{id}")
     public Mock getMock(@PathVariable Integer id) {
+        if (Objects.equals(id, 0)) {
+            throw new CustomerException("10001", "抛出自定义异常");
+        }
         return mockService.getMock(id);
     }
 }
