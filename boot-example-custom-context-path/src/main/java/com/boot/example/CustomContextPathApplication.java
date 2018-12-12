a98eeb4d1a8d17172165a54b8f7b26e54ca6c141
lipeng <lipeng@jd.com>
2018-12-12 16:29:20 +0800

    Spring Boot自定义context-path

---
+++ boot-example-custom-context-path/src/main/java/com/boot/example/CustomContextPathApplication.java
@@ -0,0 +1,18 @@
+package com.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.boot.example.CustomContextPathApplication
+ *
+ * @author lipeng
+ * @dateTime 2018/12/12 下午4:16
+ */
+@SpringBootApplication
+public class CustomContextPathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomContextPathApplication.class, args);
+    }
+}
