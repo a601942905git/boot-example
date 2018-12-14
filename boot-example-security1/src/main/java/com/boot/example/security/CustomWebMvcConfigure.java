b0204ea7ffdbc43118f6fd47a47e02eeeda98b82
lipeng <lipeng@jd.com>
2018-12-14 14:00:27 +0800

    Spring Boot集成Security

---
+++ boot-example-security1/src/main/java/com/boot/example/security/CustomWebMvcConfigure.java
@@ -0,0 +1,22 @@
+package com.boot.example.security;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * com.boot.example.security.WebMvcConfigure
+ *
+ * @author lipeng
+ * @dateTime 2018/12/14 下午1:42
+ */
+public class CustomWebMvcConfigure implements WebMvcConfigurer {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
---
+++ boot-example-security1/src/main/java/com/boot/example/security/WebSecurityConfigure.java
@@ -0,0 +1,50 @@
+package com.boot.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * com.boot.example.security.WebSecurityConfigure
+ *
+ * @author lipeng
+ * @dateTime 2018/12/14 上午11:49
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.builder()
+                        .username("root")
+                        .password("root")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
