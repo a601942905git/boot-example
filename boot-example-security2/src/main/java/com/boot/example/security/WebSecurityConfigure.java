1c4b38456901a45fa9adf2d0ba14620ea53cfaf0
lipeng <lipeng@jd.com>
2018-12-17 11:14:54 +0800

    Spring Boot集成Security

--- boot-example-security2/src/main/java/com/boot/example/security/WebSecurityConfigure.java
+++ boot-example-security2/src/main/java/com/boot/example/security/WebSecurityConfigure.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,7 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @dateTime 2018/12/14 上午11:49
  */
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
