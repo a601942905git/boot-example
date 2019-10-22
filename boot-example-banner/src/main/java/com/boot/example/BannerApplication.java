ec8d70ab570ba13f521b1b0e31796137d64f76b2
lipeng <lipeng@shein.com>
2019-10-22 11:00:57 +0800

    SpringBoot自定义banner

---
+++ boot-example-banner/src/main/java/com/boot/example/BannerApplication.java
@@ -0,0 +1,18 @@
+package com.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.boot.example.BannerApplication
+ *
+ * @author lipeng
+ * @date 2019/10/22 上午10:23
+ */
+@SpringBootApplication
+public class BannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BannerApplication.class, args);
+    }
+}
