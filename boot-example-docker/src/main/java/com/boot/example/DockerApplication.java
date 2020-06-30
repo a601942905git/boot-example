e38e29c239a8e0c80eef1c81e789e678968d2934
lipeng <lipeng@shein.com>
2020-06-30 10:56:29 +0800

    SpringBoot使用Docker进行部署

---
+++ boot-example-docker/src/main/java/com/boot/example/DockerApplication.java
@@ -0,0 +1,18 @@
+package com.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.boot.example.DockerApplication
+ *
+ * @author lipeng
+ * @date 2020/6/30 9:57 AM
+ */
+@SpringBootApplication
+public class DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+}
