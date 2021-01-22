0488f66ca27299a4b29372c526fbdfd7973018ec
lipeng <lipeng@shein.com>
2021-01-22 14:22:09 +0800

    添加注释

--- boot-example-primary/boot-example-filter/src/main/java/com/boot/example/WebFilterConfiguration.java
+++ boot-example-primary/boot-example-filter/src/main/java/com/boot/example/WebFilterConfiguration.java
@@ -13,9 +13,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebFilterConfiguration {
 
+    /**
+     * 1.org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext#getSelfInitializer()
+     * 定义ServletContextInitializer
+     *
+     * 2.org.springframework.boot.web.embedded.tomcat.TomcatStarter#onStartup(java.util.Set, javax.servlet.ServletContext)
+     * 调用ServletContextInitializer的onStartup()
+     *
+     * 3.ServletContextInitializerBeans#addServletContextInitializerBeans(ListableBeanFactory)方法会从IOC容器中获取类型为
+     * ServletContextInitializer的Bean，之后添加到ServletContext中
+     *
+     *
+     *
+     * @return FilterRegistrationBean
+     */
     @Bean
-    public FilterRegistrationBean filterRegistrationBean() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+    public FilterRegistrationBean<CustomFilter2> filterRegistrationBean() {
+        FilterRegistrationBean<CustomFilter2> filterRegistrationBean = new FilterRegistrationBean();
         filterRegistrationBean.setFilter(new CustomFilter2());
         filterRegistrationBean.addUrlPatterns("/*");
         filterRegistrationBean.setOrder(2);
