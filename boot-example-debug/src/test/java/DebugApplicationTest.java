a11fd048332de148dc9df25b63b8e389dd65fb7c
lipeng <lipeng@shein.com>
2021-01-25 16:05:31 +0800

    添加加载spring.factories文件测试方法

--- boot-example-debug/src/test/java/DebugApplicationTest.java
+++ boot-example-debug/src/test/java/DebugApplicationTest.java
@@ -2,6 +2,16 @@ import com.boot.example.DebugApplication;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.util.ClassUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
 
 /**
  * PACKAGE_NAME.DebugController
@@ -18,8 +28,35 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 public class DebugApplicationTest {
 
+    public static final String FACTORIES_RESOURCE_LOCATION = "META-INF/spring.factories";
+
     @Test
     public void test() {
         log.info("执行测试任务......");
+    }
+
+    @Test
+    public void springFactoriesTest() {
+        ClassLoader defaultClassLoader = ClassUtils.getDefaultClassLoader();
+        if (Objects.isNull(defaultClassLoader)) {
+            defaultClassLoader = DebugApplicationTest.class.getClassLoader();
+        }
+
+        try {
+            Enumeration<URL> urls = defaultClassLoader.getResources(FACTORIES_RESOURCE_LOCATION);
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                UrlResource urlResource = new UrlResource(url);
+                Properties properties = PropertiesLoaderUtils.loadProperties(urlResource);
+                for (Map.Entry<?, ?> entry : properties.entrySet()) {
+                    log.info("spring.factories key===>" + entry.getKey());
+                    log.info("spring.factories value===>" + entry.getValue());
+                }
+
+            }
+        } catch (IOException e) {
+            log.error("io exception：", e);
+        }
+
     }
 }
