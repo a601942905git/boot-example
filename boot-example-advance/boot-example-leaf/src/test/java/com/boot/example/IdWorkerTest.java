ceb984be5075e2003fc8f29cf328ba9d102387f4
10007616 <lipeng@shein.com>
2021-12-02 17:30:30 +0800

    添加注释

--- boot-example-advance/boot-example-leaf/src/test/java/com/boot/example/IdWorkerTest.java
+++ boot-example-advance/boot-example-leaf/src/test/java/com/boot/example/IdWorkerTest.java
@@ -11,6 +11,22 @@ import java.util.concurrent.TimeUnit;
 /**
  * com.boot.example.IdWorkerTest
  *
+ * 雪花算法生成18为id如下：
+ * next id：420283455004348416
+ * next id：420283455004348417
+ * next id：420283455004348418
+ * next id：420283455004348419
+ * next id：420283455004348420
+ * next id：420283455004348421
+ * next id：420283455004348422
+ * next id：420283455004348423
+ * next id：420283455004348424
+ * next id：420283455004348425
+ * next id：420283455004348426
+ * next id：420283455004348427
+ * next id：420283455004348428
+ * next id：420283455004348429
+ *
  * @author lipeng
  * @date 2021/6/29 10:25 AM
  */
