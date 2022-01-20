a5987f4a3921270dc933e63312f886696fdf11b5
10007616 <lipeng@shein.com>
2022-01-20 17:49:59 +0800

    集成docker

--- boot-example-advance/boot-example-docker/src/main/java/com/boot/example/DockerApplication.java
+++ boot-example-advance/boot-example-docker/src/main/java/com/boot/example/DockerApplication.java
@@ -2,6 +2,8 @@ package com.boot.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * com.boot.example.DockerApplication
@@ -10,7 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/6/30 9:57 AM
  */
 @SpringBootApplication
+@RestController
 public class DockerApplication {
+
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DockerApplication.class, args);
