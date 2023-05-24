f97533e9afb6b79b8e76769937fbeee6e1160aed
10007616 <lipeng@shein.com>
2023-05-24 18:52:20 +0800

    kafka发送消息增加回调

--- boot-example-mq/boot-example-kafka/src/main/java/com/boot/example/KafkaApplication.java
+++ boot-example-mq/boot-example-kafka/src/main/java/com/boot/example/KafkaApplication.java
@@ -1,11 +1,16 @@
 package com.boot.example;
 
+import org.apache.kafka.clients.ClientResponse;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.internals.AbstractCoordinator;
 import org.apache.kafka.clients.consumer.internals.ConsumerCoordinator;
+import org.apache.kafka.clients.producer.internals.Sender;
 import org.apache.kafka.common.utils.Timer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * com.boot.example.KafkaApplication
@@ -20,6 +25,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 重平衡：
  * @see AbstractCoordinator.HeartbeatResponseHandler 错误类型为：Errors.REBALANCE_IN_PROGRESS
  *
+ * 回调：
+ * @see Sender#run()
+ * @see Sender#sendProduceRequest(long, int, short, int, List)
+ * @see Sender#handleProduceResponse(ClientResponse, Map, long)
  *
  * @author lipeng
  * @date 2020/2/20 下午5:35
--- boot-example-mq/boot-example-kafka/src/main/java/com/boot/example/producer/KafkaMessageSender.java
+++ boot-example-mq/boot-example-kafka/src/main/java/com/boot/example/producer/KafkaMessageSender.java
@@ -3,7 +3,10 @@ package com.boot.example.producer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * com.boot.example.producer.KafkaSender
@@ -19,6 +22,14 @@ public class KafkaMessageSender<K, V> {
     private KafkaTemplate<K, V> kafkaTemplate;
 
     public void sendSimpleMessage(String topic, V value) {
-        kafkaTemplate.send(topic, value);
+        CompletableFuture<SendResult<K, V>> completableFuture = kafkaTemplate.send(topic, value);
+        // 异步发送消息，当发送完成后，会回调whenComplete方法
+        completableFuture.whenComplete((sendResult, throwable) -> {
+            if (throwable != null) {
+                log.error("发送消息失败，topic：{}，value：{}", topic, value, throwable);
+            } else {
+                log.info("发送消息成功，topic：{}，value：{}", topic, value);
+            }
+        });
     }
 }
